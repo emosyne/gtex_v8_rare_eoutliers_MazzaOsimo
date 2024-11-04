e7afd35fe1ce203d8f2b26192b72723ceaf640ed
emosyne <efo22@cam.ac.uk>
2024-11-04 17:15:29 +0000

    debug dependencies up to preprocessing/correction/calculate_PEER.sh - separate R and python envs

--- MazzaOsimo_eoutliers_calc.sh
+++ MazzaOsimo_eoutliers_calc.sh
@@ -63,10 +63,10 @@ head ${SAMPLE_TISSUES}
 #split expression by tissue
 echo "running split_expr_by_tissues.py"
 END='.tpm.txt'
-python2 ${scriptdir}/split_expr_by_tissues.py --GTEX $GTEX_expr --OUT $peerdir --SAMPLE $SAMPLE_TISSUES --END $END
+python2 ${scriptdir}/split_expr_by_tissues.py --gtex $GTEX_expr --out $peerdir --sample $SAMPLE_TISSUES --end $END
 
 END='.reads.txt'
-python2 ${scriptdir}/split_expr_by_tissues.py --GTEX $GTEX_expr --OUT $peerdir --SAMPLE $SAMPLE_TISSUES --END $END
+python2 ${scriptdir}/split_expr_by_tissues.py --gtex $GTEX_expr --out $peerdir --sample $SAMPLE_TISSUES --end $END
 
 ls ${peerdir} 
 
