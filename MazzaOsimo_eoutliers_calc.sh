38afb21291b17495cba0e9e4e91f935023789220
emosyne <efo22@cam.ac.uk>
2024-11-28 09:57:18 +0000

    add scripts to calculate tissue and global outliers

--- MazzaOsimo_eoutliers_calc.sh
+++ MazzaOsimo_eoutliers_calc.sh
@@ -48,7 +48,10 @@ source /home/efo22/miniconda3/etc/profile.d/conda.sh
 
 
 
-# # PART1: up to PEER correction
+# ## PART1: perform steps up and including PEER correction
+### taken from multiple files, mostly based on https://github.com/nmferraro5/gtex_v8_rare_eoutliers/blob/716d43303dd31fae20869d4957b631393fb409c5/correction.md
+
+
 # source activate expr_preprocessing_bash_py2_env
 
 # #create sample tissues file
@@ -131,9 +134,11 @@ source /home/efo22/miniconda3/etc/profile.d/conda.sh
 
 
 
-# Outlier calling 
-#     gtex_v8_rare_eoutliers / outlier_calling.md
-# ====================
+# in order to now create normalized_expression_v8ciseQTLs_removed.txt by removing tissue and global expression outliers from 
+# `preprocessing_v8/gtex_normalized_expression.txt.gz`, one needs to calculate /call outliers, originally done in a separate md file:
+
+## PART 2: Outlier calling 
+##  taken from https://github.com/nmferraro5/gtex_v8_rare_eoutliers/blob/716d43303dd31fae20869d4957b631393fb409c5/outlier_calling.md
 
 
 ### Run outlier calling
@@ -145,8 +150,13 @@ Rscript outlier_calling/call_outliers.R \
         --OUT.PREFIX=/data_v8/outliers/gtexV8.outlier.controls.v8ciseQTLs.globalOutliers.removed \
         --N.PHEN=5 
 
+# Generates one file in specified directory with columns gene ind N Df MedZ Y, where N refers to the number of individuals tested for a given gene, 
+# Df refers to the number of measurements available for that gene in that individual and Y indicates outlier or control. 
+# The `N.PHEN` argument specifies the minimum number of measurements (i.e. tissues) available for a gene-individual required for inclusion. 
+# We use 5. `GLOBAL` specifies a file with a list of individual IDs to remove as global outliers. If NA, no individuals are removed.
 
-Generates one file in specified directory with columns gene ind N Df MedZ Y, where N refers to the number of individuals tested for a given gene, Df refers to the number of measurements available for that gene in that individual and Y indicates outlier or control. The `N.PHEN` argument specifies the minimum number of measurements (i.e. tissues) available for a gene-individual required for inclusion. We use 5. `GLOBAL` specifies a file with a list of individual IDs to remove as global outliers. If NA, no individuals are removed.
+
+
 
 ### Identify global outlier individuals 
 
@@ -165,8 +175,9 @@ Rscript outlier_calling/extract_extreme_tissues.R \
 
 Generates figures in `figures/GTEXv8_pair_jaccard.pdf`.
 
-### Select tissues and individuals for downstream analyses
-Rscript preprocessing/filter_tissues_individuals.R
-# Must be run from the upper level directory of the repo (e.g., the location of this readme).
-# Generates `preprocessing_v8/gtex_2017-06-05_v8_design_passed.txt`, `preprocessing_v8/gtex_2017-06-05_v8_individuals_passed.txt`, `preprocessing_v8/gtex_2017-06-05_v8_tissues_passed.txt`, and `preprocessing_v8/gtex_2017-06-05_v8_normalized_expression.subset.txt.gz`. Also produces summary figures `figures/gtex_v8_design.pdf`. The subset file filtered for missingness is used in correlation-outlier calling. No missingness filter is applied for multi-tissue eOutlier calling.
+
+# ### Select tissues and individuals for downstream analyses (still from correction.md)
+# Rscript preprocessing/filter_tissues_individuals.R
+# # Must be run from the upper level directory of the repo (e.g., the location of this readme).
+# # Generates `preprocessing_v8/gtex_2017-06-05_v8_design_passed.txt`, `preprocessing_v8/gtex_2017-06-05_v8_individuals_passed.txt`, `preprocessing_v8/gtex_2017-06-05_v8_tissues_passed.txt`, and `preprocessing_v8/gtex_2017-06-05_v8_normalized_expression.subset.txt.gz`. Also produces summary figures `figures/gtex_v8_design.pdf`. The subset file filtered for missingness is used in correlation-outlier calling. No missingness filter is applied for multi-tissue eOutlier calling.
 
